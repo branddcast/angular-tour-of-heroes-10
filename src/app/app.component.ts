49cb64defaffc5501e22a1f3977ba5984d252cc6
Brandon Jaime <brandon.jaime.castillo@banorte.com>
2022-06-22 11:26:55 -0500

    Fix funcion web view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes10';
+  /*host = environment.host;
+  api = environment.api;
+  env = environment.env;
+  user = environment.user;
+  username = environment.username;*/
 }
